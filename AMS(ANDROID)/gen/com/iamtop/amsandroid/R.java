b798de4660c0157a1b3eb4c02c02be45d390b8e9
Pabitra1 <pabitranou1@gmail.com>
2015-05-28 10:51:47 +0800

    litepal configuration

--- AMS(ANDROID)/gen/com/iamtop/amsandroid/R.java
+++ AMS(ANDROID)/gen/com/iamtop/amsandroid/R.java
@@ -1092,15 +1092,17 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
-        public static final int dept1=0x7f05003c;
-        public static final int dept2=0x7f05003d;
+        public static final int dept1=0x7f05003e;
+        public static final int dept2=0x7f05003f;
+        public static final int dept_code=0x7f05003c;
+        public static final int dept_name=0x7f05003d;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
@@ -1195,8 +1197,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_login=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_batch=0x7f030018;
+        public static final int activity_login=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int login=0x7f0c0000;
