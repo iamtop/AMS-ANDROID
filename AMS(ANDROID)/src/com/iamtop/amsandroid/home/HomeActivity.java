c52e220a54fbcfffbd16981c7e8663065e0fda0c
Pabitra1 <pabitranou1@gmail.com>
2015-06-01 16:01:50 +0800

    Header and footer page in home page.

--- AMS(ANDROID)/src/com/iamtop/amsandroid/home/HomeActivity.java
+++ AMS(ANDROID)/src/com/iamtop/amsandroid/home/HomeActivity.java
@@ -1,25 +1,56 @@
 package com.iamtop.amsandroid.home;
 
 import com.iamtop.amsandroid.R;
+import com.iamtop.amsandroid.login.LoginActivity;
 
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		ActionBar actionBar = getActionBar();
+		actionBar.show();
+		
 	}
+	
+	public void goToManagement(View v){
+		
+	}
+	
+   public void goToTaskManager(View v){
+		
+	}
+   public void goToAttendance(View v){
+		
+	}
+   public void goToWebsite(View v){
+	  
+	   Intent i = new Intent(Intent.ACTION_VIEW, 
+		       Uri.parse("http://iamtop.net/"));
+		startActivity(i);
+		
+	}
+   
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
+		
+		MenuItem logoutitem = menu.findItem(R.id.log_out);
 		return true;
 	}
 
@@ -28,10 +59,15 @@ public class HomeActivity extends ActionBarActivity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		switch (item.getItemId()) {
+		case R.id.log_out:
+			
+			Intent intent=new Intent(HomeActivity.this,LoginActivity.class);
+			startActivity(intent);
 			return true;
-		}
+		
+	default:
 		return super.onOptionsItemSelected(item);
+		}
 	}
 }
